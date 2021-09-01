d37c12b3036647c293c43fa74ffb98c536d06516
dahead <dahead@mailbox.org>
2021-09-01 16:15:50 +0200

    Updates.

--- Core/TodoItem.cs
+++ Core/TodoItem.cs
@@ -97,6 +97,25 @@ public class TodoItem
 		// var total = (passed.Ticks + rest.Ticks) / rest.Ticks;
 		// return total;
 	}
+	static string GetAgeOfStory(DateTimeOffset createdAt)
+	{
+		var timespanSinceStoryCreated = DateTimeOffset.UtcNow - createdAt;
+
+		return timespanSinceStoryCreated switch
+		{
+			TimeSpan storyAge when storyAge < TimeSpan.FromHours(1) => $"{Math.Ceiling(timespanSinceStoryCreated.TotalMinutes)} minutes",
+
+			TimeSpan storyAge when storyAge >= TimeSpan.FromHours(1) && storyAge < TimeSpan.FromHours(2) => $"{Math.Floor(timespanSinceStoryCreated.TotalHours)} hour",
+
+			TimeSpan storyAge when storyAge >= TimeSpan.FromHours(2) && storyAge < TimeSpan.FromHours(24) => $"{Math.Floor(timespanSinceStoryCreated.TotalHours)} hours",
+
+			TimeSpan storyAge when storyAge >= TimeSpan.FromHours(24) && storyAge < TimeSpan.FromHours(48) => $"{Math.Floor(timespanSinceStoryCreated.TotalDays)} day",
+
+			TimeSpan storyAge when storyAge >= TimeSpan.FromHours(48) => $"{Math.Floor(timespanSinceStoryCreated.TotalDays)} days",
+
+			_ => string.Empty,
+		};
+	}
 
 	public string GetRemainingTimePercentString()
 	{
